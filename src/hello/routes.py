aba3fc2b0594fec311137a4d9465ccfe8ad0af8a
Kesavan Trichy Rengarajan <k7@trk7.com>
2023-09-02 22:50:35 +1000

    Add deploy step to production

--- src/hello/routes.py
+++ src/hello/routes.py
@@ -4,4 +4,4 @@ route_blueprint = Blueprint('route_blueprint', __name__)
 
 @route_blueprint.route("/")
 def hello_world():
-    return f"<h1>Hello, World!</h1><h2>From <span style='color:red;text-decoration:underline'>{os.getenv('APP_ENV', 'Unknown ')}</span> enviroment.</h2>"
+    return f"<h1>Hello, World! From CI/CD.</h1><h2>From <span style='color:red;text-decoration:underline'>{os.getenv('APP_ENV', 'Unknown ')}</span> enviroment.</h2>"
